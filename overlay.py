d73892b190c5abeeebd2123c7484455f87a6aec8
Meta Novitia <mnovitia@uci.edu>
2019-12-03 15:54:39 -0800

    warnings

--- overlay.py
+++ overlay.py
@@ -109,10 +109,12 @@ highestHR = 200
 
 img_size        = (500,90)     
 screen_size     = (1280,720)
+warn_size       = (1260,700)
 temp_text_pos   = (1050,500)
 hr_text_pos     = (1050,580)
 heart_pos       = (980,510)
 thermo_pos      = (980,590)
+warning_x       = 1160
 font_size       = 50
 
 #initialze camera
@@ -128,10 +130,19 @@ temp_text   = ImageDraw.Draw(img)
 img2        = Image.new('RGBA',img_size,color=Color4.transparent)
 hr_text     = ImageDraw.Draw(img2)
 
+warn        = Image.new('RGBA',screen_size,color=Color4.transparent)
+warn_draw   = ImageDraw.Draw(warn)
+warn_draw.rectangle([(0,0),warn_size],fill=None, outline=Color3.yellow)
+
 
 heart       = Image.open("heart.png").resize((50,50))
 thermo      = Image.open("thermo.png").resize((50,50))
+warning     = Image.open("warning.png").resize((50,50))
 pad         = Image.new('RGBA', screen_size)
+
+masks       = []
+for i in range(10):
+    masks.append(Image.open("mask"+str(i)+".png").resize((55,50)).convert("L"))
 
 #initialize image overlay
 pad.paste(img, hr_text_pos)
@@ -169,12 +180,20 @@ while(True):
     try: temperature = getTemperature(ADC_temp, rref)
     except ZeroDivisionError: pass
     
-    
     # clear and redraw
     temp_text.rectangle([(0,0),img_size],fill=Color4.transparent)
     temp_text.text((0,0),str(int(temperature)), font=font, fill=Color3.black)
     hr_text.rectangle([(0,0),img_size],fill=Color4.transparent)
     hr_text.text((0,0),str(int(heartrate)), font=font, fill=Color3.black)
+
+    if temperature > 140 or heartrate > 200:
+        pad.paste(warn, (0,0))
+    if temperature > 140:
+        warning.putalpha(masks[int(time()*10)%10])
+        pad.paste(warning, (warning_x,thermo_pos[1]))
+    if heartrate > 200:
+        warning.putalpha(masks[int(time()*10)%10])
+        pad.paste(warning, (warning_x,heart_pos[1]))
 
     # create overlay using drawing
     pad.paste(img, hr_text_pos)
--- temp.py
+++ temp.py
@@ -11,12 +11,14 @@ CS      = 25
 channel = 0
 rref    = 1000
 
-img_size        = (500,90)
+img_size        = (200,90)
 screen_size     = (1280,720)
+warn_size     = (1260,700)
 temp_text_pos   = (1050,500)
 heart_text_pos  = (1050,580)
 thermo_pos      = (980,510)
 heart_pos       = (980,590)
+warning_x       = (1160)
 font_size       = 50
 
 font        = ImageFont.truetype("OpenSans-Regular.ttf",size=font_size)
@@ -25,9 +27,18 @@ temp_text   = ImageDraw.Draw(img)
 img2        = Image.new('RGBA',img_size,color=Color4.transparent)
 heart_text  = ImageDraw.Draw(img2)
 
+warn        = Image.new('RGBA',screen_size,color=Color4.transparent)
+warn_draw   = ImageDraw.Draw(warn)
+warn_draw.rectangle([(10,10),warn_size],fill=None, outline=Color3.yellow)
+
 heart       = Image.open("heart.png").resize((50,50))
 thermo      = Image.open("thermo.png").resize((50,50))
+warning     = Image.open("warning.png").resize((55,50))
 pad         = Image.new('RGBA', screen_size)
+
+masks       = []
+for i in range(10):
+    masks.append(Image.open("mask"+str(i)+".png").resize((55,50)).convert("L"))
 
 prev_temperature    = -1
 prev_heartrate      = -1
@@ -40,11 +51,6 @@ doInit              = True
 # ADC_value = readAdc(channel, CLK, MISO, MOSI, CS)
 temperature         = str(random.randint(150,160))
 heartrate           = str(random.randint(160,170))
-if doInit:
-    prev_temperature    = temperature
-    prev_heartrate      = heartrate
-temperature_delta   = temperature-prev_temperature
-heartrate_delta     = heartrate-prev_heartrate
 
 sleep(0.1)
 
@@ -55,10 +61,15 @@ heart_text.rectangle([(0,0),img_size],fill=Color4.transparent)
 heart_text.text((0,0),str(int(heartrate)), font=font, fill=Color3.black)
 
 # create overlay using drawing
+pad.paste(warn, (0,0))
 pad.paste(img, temp_text_pos)
 pad.paste(img2, heart_text_pos)
 pad.paste(heart, heart_pos)
 pad.paste(thermo, thermo_pos)
+warning.putalpha(masks[3])
+pad.paste(warning, (warning_x,thermo_pos[1]))
+warning.putalpha(masks[7])
+pad.paste(warning, (warning_x,heart_pos[1]))
 pad.save("new.png")
 
 prev_temperature = temperature
